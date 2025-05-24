2cea2d824ef26ebb30f6324eb08fdda42a7468b4
Lucas Winkler <lucaswinkler@gmail.com>
2025-05-24 03:04:47 +0000

    Setup Prettier, ESLint, Husky and workspace settings

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['node_modules', '.prettierignore'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'import/order': 'off', // Conflicts with prettier import order plugin
+      'sort-imports': 'off', // Conflicts with prettier import order plugin
+    },
+  }
+);
