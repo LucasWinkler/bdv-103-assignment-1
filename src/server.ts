4df90907f9616cb09e0f1c26cfa86754e18cf657
Lucas Winkler <lucaswinkler@gmail.com>
2025-06-30 21:23:37 +0000

    Move server setup to serverLauncher.ts

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,5 @@
-import cors from '@koa/cors';
-import KoaRouter from '@koa/router';
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
-import logger from 'koa-logger';
-import qs from 'koa-qs';
-import { koaSwagger } from 'koa2-swagger-ui';
+import { createServer } from './serverLauncher';
 
-import { RegisterRoutes } from '../build/routes';
-import swagger from '../build/swagger.json';
-import zodRouter from './routes';
+const PORT = Number(process.env.PORT) || 3000;
 
-const app = new Koa();
-const koaRouter = new KoaRouter();
-
-qs(app);
-
-app.use(logger());
-app.use(
-  cors({
-    origin: 'http://localhost:9080',
-  })
-);
-app.use(bodyParser());
-
-// Old routes
-app.use(zodRouter.routes());
-app.use(zodRouter.allowedMethods());
-
-// New routes
-RegisterRoutes(koaRouter);
-app.use(koaRouter.routes());
-app.use(koaRouter.allowedMethods());
-
-app.use(
-  koaSwagger({
-    routePrefix: '/docs',
-    specPrefix: '/docs/spec',
-    exposeSpec: true,
-    swaggerOptions: { spec: swagger },
-  })
-);
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+createServer(PORT);
---
+++ src/serverLauncher.ts
@@ -0,0 +1,56 @@
+import { Server } from 'http';
+
+import cors from '@koa/cors';
+import KoaRouter from '@koa/router';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
+import qs from 'koa-qs';
+import { koaSwagger } from 'koa2-swagger-ui';
+
+import { RegisterRoutes } from '../build/routes';
+import swagger from '../build/swagger.json';
+import zodRouter from './routes';
+
+export function createServer(port: number = 0): Server {
+  const app = new Koa();
+  const koaRouter = new KoaRouter();
+
+  qs(app);
+
+  app.use(logger());
+  app.use(
+    cors({
+      origin: 'http://localhost:9080',
+    })
+  );
+  app.use(bodyParser());
+
+  // Old routes
+  app.use(zodRouter.routes());
+  app.use(zodRouter.allowedMethods());
+
+  // New routes
+  RegisterRoutes(koaRouter);
+  app.use(koaRouter.routes());
+  app.use(koaRouter.allowedMethods());
+
+  app.use(
+    koaSwagger({
+      routePrefix: '/docs',
+      specPrefix: '/docs/spec',
+      exposeSpec: true,
+      swaggerOptions: { spec: swagger },
+    })
+  );
+
+  const server = app.listen(port, () => {
+    const address = server.address();
+    const actualPort =
+      typeof address === 'object' && address ? address.port : port;
+
+    console.log(`Server running on http://localhost:${actualPort}`);
+  });
+
+  return server;
+}
