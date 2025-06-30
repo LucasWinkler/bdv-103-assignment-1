8390be285159ca473b0113b45553307652f1b8d4
Lucas Winkler <lucaswinkler@gmail.com>
2025-06-30 22:17:39 +0000

    Add address context to hello test

--- tests/hello.spec.ts
+++ tests/hello.spec.ts
@@ -2,11 +2,13 @@ import { describe, expect, it } from 'vitest';
 
 import { setupTestServer } from './startServer';
 
+import type { TestContext } from './startServer';
+
 setupTestServer();
 
 describe('Hello', () => {
-  it('should return status `200` and message `Hello World`', async () => {
-    const response = await fetch('http://localhost:3000/hello/World');
+  it('should return status `200` and message `Hello World`', async (context: TestContext) => {
+    const response = await fetch(`${context.address}/hello/World`);
     const body = await response.json();
     expect(response.status).toBe(200);
     expect(body.message).toBe('Hello World');
