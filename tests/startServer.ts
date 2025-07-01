a804a9014adb310d3633454fa7e060db067df5e0
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-01 01:10:34 +0000

    Update startServer context to contain AppState

--- tests/startServer.ts
+++ tests/startServer.ts
@@ -2,24 +2,24 @@ import { afterEach, beforeEach } from 'vitest';
 
 import { createServer } from '../src/server';
 
-import type { Server } from 'http';
+import type { AppState } from '../src/types/state';
 
 export type TestContext = {
   address: string;
   close: () => void;
+  state: AppState;
 };
 
-let server: Server;
-
-export function setupTestServer() {
-  beforeEach<TestContext>((context) => {
-    server = createServer();
+export async function setupTestServer() {
+  beforeEach<TestContext>(async (context) => {
+    const { server, state } = await createServer();
 
     const address = server.address();
     const port = typeof address === 'object' && address ? address.port : 0;
 
     context.address = `http://localhost:${port}`;
     context.close = () => server.close();
+    context.state = state;
   });
 
   afterEach<TestContext>((context) => {
